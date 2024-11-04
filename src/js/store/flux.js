7f05f4ce04f9d83c2d28153f8514829e32438dbf
Jean Olmedillo <78714438+Jmofuture@users.noreply.github.com>
2024-11-04 21:40:03 +0000

    Fixed function planet

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -7,29 +7,21 @@ const getState = ({ getStore, getActions, setStore }) => {
             favorites: [],
         },
         actions: {
-            // Añadir favorito
+
             addFav: (fav) => {
                 const store = getStore();
                 if (!store.favorites.some(f => f.uid === fav.uid)) {
                     setStore({ favorites: [...store.favorites, fav] });
                 }
             },
-            // Eliminar favorito por UID
+
             removeFavByUid: (uid) => {
                 const store = getStore();
                 const updatedFavorites = store.favorites.filter(fav => fav.uid !== uid);
                 setStore({ favorites: updatedFavorites });
             },
-            // Cambiar color
-            changeColor: (index, color) => {
-                const store = getStore();
-                const demo = store.demo.map((elm, i) => {
-                    if (i === index) elm.background = color;
-                    return elm;
-                });
-                setStore({ demo: demo });
-            },
-            // Función genérica para fetch
+
+
             fetchData: (url, setter) => {
                 fetch(url)
                     .then(response => {
@@ -46,26 +38,19 @@ const getState = ({ getStore, getActions, setStore }) => {
                         console.error("Hubo un error:", error);
                     });
             },
-            // Obtener personajes
+
             getPeople: () => {
                 getActions().fetchData("https://www.swapi.tech/api/people", 'characters');
             },
-            // Obtener vehículos
+
             getVehicles: () => {
                 getActions().fetchData("https://www.swapi.tech/api/vehicles", 'vehicles');
             },
-            // Obtener planetas
+ 
             getPlanets: () => {
                 getActions().fetchData("https://www.swapi.tech/api/planets", 'planets');
             },
-            // Ejemplo de función
-            exampleFunction: () => {
-                getActions().changeColor(0, "green");
-            },
-            // Cargar algunos datos (puedes implementar tu propia lógica aquí)
-            loadSomeData: () => {
-                // fetch().then().then(data => setStore({ "foo": data.bar }))
-            },
+           
         }
     };
 };
